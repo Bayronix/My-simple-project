a5be45dea2e52f211227f379b51b5d6423894115
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-04-02 12:02:02 +0200

    .

--- code.js
+++ code.js
@@ -668,34 +668,44 @@
 
 // return average;
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  {
-    title: "The Dreams in the Witch House",
-    author: "Howard Lovecraft",
-    rating: 8.67,
-  },
-];
-const MIN_BOOK_RATING = 8;
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
 
-const names = books
-  .filter((a) => a.rating >= MIN_BOOK_RATING)
-  .toSorted((a, b) => a.rating - b.rating)
-  .map((a) => a.author);
+// const names = books
+//   .filter((a) => a.rating >= MIN_BOOK_RATING)
+//   .toSorted((a, b) => a.rating - b.rating)
+//   .map((a) => a.author);
 
-console.log(names);
+// console.log(names);
+
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+const getWage = (baseSalary, overtime, rate) => {
+  return baseSalary + overtime * rate;
+};
+
+console.log(getWage(baseSalary, overtime, rate));

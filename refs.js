c927c36c317b8d983816481b287321e13f829253
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-05-24 17:18:49 +0200

    .

--- refs.js
+++ refs.js
@@ -1,5 +1,13 @@
-export const refs = {
-  buttonIncrement: document.querySelector(`button[data-action='increment']`),
-  buttonValue: document.querySelector(".counter-value"),
-  buttonDecrement: document.querySelector(`button[data-action='decrement']`),
+// export const refs = {
+//   buttonIncrement: document.querySelector(`button[data-action='increment']`),
+//   buttonValue: document.querySelector(".counter-value"),
+//   buttonDecrement: document.querySelector(`button[data-action='decrement']`),
+// };
+const fetchUsers = async () => {
+  const response = await axios.get(
+    "<https://jsonplaceholder.typicode.com/users>"
+  );
+  return response.data;
 };
+
+fetchUsers().then((users) => console.log(users));
